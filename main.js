a851a476bb46fcc5fc72e98576adc3835a5eb943
satriazoid <satriaalfa77@gmail.com>
2025-07-23 14:56:44 +0700

    add varibel

---
+++ main.js
@@ -0,0 +1,9 @@
+let nama = 'anton', umur = 17, alamat = 'klaten';
+
+const motorpertama ='Vixion';
+
+a = 10;
+var a;
+document.write(alamat); // maka hasil nya sesuai dengan let di atas
+document.write('<br/>');
+document.write(a); // maka hasil nya sesuai dengan let di atas
